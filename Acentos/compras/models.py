34a1cc80ac84955aad9ca38956d0ab709f8bd1fa
Isaac <isaactadinagiraldo02@gmail.com>
2021-09-28 16:39:08 -0500

    creación modelo carrito.

--- Acentos/compras/models.py
+++ Acentos/compras/models.py
@@ -1,3 +1,13 @@
+"""Compras modelos"""
+
+# Django
 from django.db import models
 
 # Create your models here.
+class carrito(models.Model):
+    "Modelo carrito"
+
+    precio = models.DecimalField(max_digits=7, decimal_places=3)
+
+
+    
