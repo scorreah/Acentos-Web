bf867f250438cdef4c1210b7aee8c2807b6e622c
scorreah <scorreah@eafit.edu.co>
2021-09-27 19:27:00 -0500

    Creación modelo Libro

--- Acentos/libros/models.py
+++ Acentos/libros/models.py
@@ -1,3 +1,12 @@
+"""Libros models."""
+
+# Django
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    """Modelo libro."""
+
+    ISBN = models.CharField(max_length=13)
+    sinopsis = models.TextField(blank=True)
+    
