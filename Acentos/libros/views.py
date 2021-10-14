aa4427c17ac639a59f997ec41e94f9ac74a6ba75
jgomezb11 <jgomezb11@eafit.edu.co>
2021-10-14 12:20:13 -0500

    Libros recomendados por editorial Ejemplo de comentarios Agrego combobox para ordenar

--- Acentos/libros/views.py
+++ Acentos/libros/views.py
@@ -12,8 +12,12 @@ def detalles(request, titulo):
     libro = Libro.objects.filter(url_libro__exact=titulo)
     libro = list(libro.values())
     libro = libro[0]
+    recomendados = Libro.objects.filter(editorial__icontains=libro['editorial'])
+
     return render(
         request=request, 
         template_name='libros/detalles.html',
-        context={'libro': libro, 'titulo': titulo}
+        context={'libro': libro, 'titulo': titulo, 'recomendados': recomendados}
     )
+
+
