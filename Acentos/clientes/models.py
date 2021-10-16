a7f5fef8de719f040bd1bbbe571125d13b8b4f6b
scorreah <scorreah@eafit.edu.co>
2021-10-16 13:00:08 -0500

    Migraciones BD y replanteamiento de modelos.

--- Acentos/clientes/models.py
+++ Acentos/clientes/models.py
@@ -1,27 +1,11 @@
 
 # Django
-from django.db import models
-from django.contrib.auth.models import User
+# from django.db import models
+# from django.contrib.auth.models import User
 
-# Models
-from compras.models import Carrito
-from libros.models import Libro
+# # Models
+# from compras.models import Carrito
+# from libros.models import Libro
 
 # Create your models here.
-class Cliente (models.Model):
-    """Modelo de cliente."""
 
-    user = models.OneToOneField(User, models.CASCADE)
-
-    ni = models.CharField(max_length=13, unique=True)
-
-    direccion = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=20, blank=True)
-
-    fecha_registro = models.DateTimeField(auto_now_add=True)
-
-    carrito = models.OneToOneField(Carrito, models.CASCADE)
-    clientes = models.ManyToManyField(Libro)
-
-    class Meta:
-        db_table = "Cliente"
--- Acentos/compras/models.py
+++ Acentos/compras/models.py
@@ -2,10 +2,10 @@
 
 # Django
 from django.db import models
+from django.contrib.auth.models import User
 
 # Models
 from libros.models import Libro
-from clientes.models import Cliente
 
 # Create your models here.
 class Carrito(models.Model):
@@ -24,6 +24,24 @@ class LibroCarrito(models.Model):
 
     class Meta:
         db_table = "LibroCarrito"
+
+class Cliente (models.Model):
+    """Modelo de cliente."""
+
+    user = models.OneToOneField(User, models.CASCADE)
+
+    ni = models.CharField(max_length=13, unique=True)
+
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20, blank=True)
+
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    carrito = models.OneToOneField(Carrito, models.CASCADE)
+    libros = models.ManyToManyField(Libro)
+
+    class Meta:
+        db_table = "Cliente"
 
 class Compra(models.Model):
     metodo_pago = models.CharField(max_length=25)
