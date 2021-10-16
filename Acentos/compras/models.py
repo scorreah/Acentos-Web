1dd7271adf416cd1a7a2ff4a5fa13e1fa0ad22df
scorreah <scorreah@eafit.edu.co>
2021-10-15 22:01:16 -0500

    Creacion y modificacion de modelos de BD. - Creacion Autor - Creacion Reseña - Creacion Cliente - Creacion LibroCarrito - Modificacion Libro - Modificacion Carrito - Renombre de todas las tablas Creacion de relaciones entre tablas. Migraciones en BD.

--- Acentos/compras/models.py
+++ Acentos/compras/models.py
@@ -3,11 +3,24 @@
 # Django
 from django.db import models
 
+# Models
+from libros.models import Libro
+
 # Create your models here.
-class carrito(models.Model):
-    "Modelo carrito"
+class Carrito(models.Model):
+    "Modelo Carrito"
 
-    precio = models.DecimalField(max_digits=7, decimal_places=3)
+    precio = models.IntegerField() 
+    libros = models.ManyToManyField(Libro, through="LibroCarrito")
 
+    class Meta:
+        db_table = "Carrito"
 
+class LibroCarrito(models.Model):
+    carrito_id = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    libro_id = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(default=1)
+
+    class Meta:
+        db_table = "LibroCarrito"
     
