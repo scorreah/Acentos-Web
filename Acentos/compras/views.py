855d0623a421aa75b772b133f990bbd22fc0a364
jgomezb11 <jgomezb11@eafit.edu.co>
2021-10-24 02:02:11 -0500

    Funcionalidad anadir y borrar del carrito

--- Acentos/compras/views.py
+++ Acentos/compras/views.py
@@ -1,13 +1,41 @@
 """Compras views."""
 # Django
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+from libros.models import Libro
+from compras.models import Carrito, LibroCarrito, Cliente, Compra
+from usuarios.models import User
 
 # Create your views here.
 def carrito(request):
     """Se encarga de los detalles del carrito."""
-    return render(request=request, template_name='compras/carrito.html')
+    userInstance = User.objects.get(username__exact="JulioLee")    
+    user = Cliente.objects.get(user__exact=userInstance)
+    libros = user.carrito.libros.all()
+
+    return render(request=request, template_name='compras/carrito.html', context={'libros': libros})
 
 def compra(request):
     """Se encarga de confirmar la compra y seleccionar los métodos de envio y pago"""
     return render(request=request,template_name='compras/compra.html')
+#@login_required
+def anadirCarrito(request, titulo):
+    """Se encarga de funcionalidad de anadir al carrito."""
+    libroInstance = Libro.objects.get(titulo__exact=titulo)
+    userInstance = User.objects.get(username__exact="JulioLee")    
+    user = Cliente.objects.get(user__exact=userInstance)
+    user.carrito.libros.add(libroInstance)
+
+    
+    return redirect ('busquedas:resultados')
+
+def eliminarCarrito(request, titulo):
+    libroInstance = Libro.objects.get(titulo__exact=titulo)
+    userInstance = User.objects.get(username__exact="JulioLee")
+    user = Cliente.objects.get(user__exact=userInstance)
+    libroEliminar = user.carrito.librocarrito_set.all()
+    libroEliminar.delete()
+    return redirect ('compras:carrito')
+
 
