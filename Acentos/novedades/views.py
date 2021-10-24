ed8ea48299e16ad1ed214c155f436fc6c93a502b
Isaac <isaactadinagiraldo02@gmail.com>
2021-10-23 23:13:27 -0500

    Correción. Vista home. Parte nuevos lanzamientos. Parte preventa.

--- Acentos/novedades/views.py
+++ Acentos/novedades/views.py
@@ -1,13 +1,49 @@
 """Novedaes views"""
+
 #Django
+
 from django.shortcuts import render
+
+from django.urls import conf
+
+import libros
+
+
+
+from libros.models import Libro
+
+
 
 # Create your views here.
 
+
+
 def error_404(request,exception):
+
     """Muestra la vista por defecto del error 404 o cuando se accede a una página que no está disponible en el dominio web."""
+
     return render(request=request,template_name='404.html')
 
-def home(request):
+
+
+def home(request,*args, **kwargs):
+
     """Muestra la vista para que se logee el cliente"""
-    return render(request=request,template_name='novedades/home.html')
+
+    global nuevos, prev
+
+    nuevos = Libro.objects.filter(nuevo=True)
+
+    prev = Libro.objects.filter(preventa=True)
+
+    return render(request=request,
+
+    template_name='novedades/home.html',
+
+    context = {
+
+        'nuevos': nuevos,
+
+        'preventa': prev,
+
+    })
