2526a79ee8eb1f33f5cc6e7e0ca8324e1483b731
scorreah <scorreah@eafit.edu.co>
2021-10-25 06:30:31 -0500

    Registro de libros en admin

--- Acentos/libros/admin.py
+++ Acentos/libros/admin.py
@@ -11,4 +11,11 @@ from libros.models import Libro
 class LibroAdmin(admin.ModelAdmin):
     """Libro admin."""
 
-    
+    list_display = ('ISBN', 'titulo', 'editorial', 'categoria', 'idioma', 'presentacion', 'fecha_publicacion', 'precio')
+    list_display_links = ('ISBN', 'titulo')
+    list_editable = ('categoria', 'idioma', 'presentacion', 'fecha_publicacion', 'precio')
+
+    search_fields = ('ISBN', 'titulo', 'autor__nombre')
+    list_filter = ('precio', 'nuevo', 'preventa', 'puntuacion', 'fecha_publicacion')
+
+    readonly_fields = ('ISBN',)
