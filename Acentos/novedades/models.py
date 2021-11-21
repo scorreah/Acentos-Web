035de8b37ccf498d219dd3018635c27a1611d842
juan9572 <jprinconu@eafit.edu.co>
2021-11-21 11:38:21 -0500

    Agrego eventos Modifico datos de la compra

--- Acentos/novedades/models.py
+++ Acentos/novedades/models.py
@@ -1,3 +1,21 @@
+""""Modelos de Eventos"""
+
+# Django
 from django.db import models
 
+# Models
+#from usuarios.models import User
+
 # Create your models here.
+class Evento(models.Model):
+    """Modelo de evento."""
+    id = models.AutoField(primary_key=True)
+    #admin = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) quien sabe
+    fechaInicio = models.DateTimeField()
+    descripcion = models.TextField()
+    fechaFin = models.DateTimeField()
+    portada = models.ImageField(
+        upload_to='libros/pictures', #Cambiar esto por una carpeta que se llame eventos/pictures
+    )
+    class Meta:
+        db_table = "Evento"
--- Acentos/novedades/views.py
+++ Acentos/novedades/views.py
@@ -3,47 +3,26 @@
 #Django
 
 from django.shortcuts import render
-
 from django.urls import conf
-
 import libros
-
-
-
 from libros.models import Libro
-
-
+from novedades.models import Evento
 
 # Create your views here.
-
-
-
 def error_404(request,exception):
-
     """Muestra la vista por defecto del error 404 o cuando se accede a una página que no está disponible en el dominio web."""
-
     return render(request=request,template_name='404.html')
 
-
-
 def home(request,*args, **kwargs):
-
     """Muestra la vista para que se logee el cliente"""
-
     global nuevos, prev
-
     nuevos = Libro.objects.filter(nuevo=True)
-
     prev = Libro.objects.filter(preventa=True)
-
+    carousel = Evento.objects.all()
     return render(request=request,
-
-    template_name='novedades/home.html',
-
-    context = {
-
-        'nuevos': nuevos,
-
-        'preventa': prev,
-
-    })
+                  template_name='novedades/home.html',
+                  context = {
+                  'nuevos': nuevos,
+                  'preventa': prev,
+                  'carousel':carousel
+                  })
